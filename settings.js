93b1ec272aac3440dcc0468bd6561999e612a845
chamodlakshan160-ux <chamodlakshan160@gmail.com>
2025-11-10 18:02:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '559bc823e08570cd' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '45efb836cf5b4bbc' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
